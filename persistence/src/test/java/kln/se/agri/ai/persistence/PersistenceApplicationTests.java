d66efb19cbc5be3ea247edacd6513689ac89170c
prasad_shalika <prasad.shalika@mvv.se>
2022-07-02 19:31:45 +0530

    Fix issues initial happen

---
+++ persistence/src/test/java/kln/se/agri/ai/persistence/PersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package kln.se.agri.ai.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
